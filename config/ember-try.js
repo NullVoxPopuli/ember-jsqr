bdfe821ef1fc1389890f8f2e1a00444350569574
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-02-08 03:07:16 -0500

    whoops

--- config/ember-try.js
+++ config/ember-try.js
@@ -6,18 +6,10 @@ module.exports = async function() {
   return {
     scenarios: [
       {
-        name: 'ember-lts-3.8',
+        name: 'ember-3.13',
         npm: {
           devDependencies: {
-            'ember-source': '~3.8.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.12',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.12.0',
+            'ember-source': '~3.13.0',
           },
         },
       },
@@ -53,34 +45,6 @@ module.exports = async function() {
         name: 'ember-default',
         npm: {
           devDependencies: {},
-        },
-      },
-      {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1',
-          },
-        },
-      },
-      {
-        name: 'ember-classic',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'application-template-wrapper': true,
-            'default-async-observers': false,
-            'template-only-glimmer-components': false,
-          }),
-        },
-        npm: {
-          ember: {
-            edition: 'classic',
-          },
         },
       },
     ],

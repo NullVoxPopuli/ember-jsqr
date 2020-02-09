167a30797e4bfa6ed64328a6a6240942f4188d61
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-02-08 22:48:24 -0500

    huh?

--- config/deploy.js
+++ config/deploy.js
@@ -5,11 +5,11 @@ module.exports = function(deployTarget) {
   let ENV = {
     build: {},
 
-    git: {
-      enabled: true,
-      repo: 'git@github.com:NullVoxPopuli/ember-jsqr.git',
-      destDir: 'dist',
-    },
+    // git: {
+    //   enabled: true,
+    //   repo: 'git@github.com:NullVoxPopuli/ember-jsqr.git',
+    //   destDir: 'dist',
+    // },
     // 'git-ci': {
     //   enabled: true,
     // },

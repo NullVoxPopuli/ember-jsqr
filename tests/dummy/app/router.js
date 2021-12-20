36d9b821a52b7ff8eefa9fd88554caf953229cb6
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-12-20 18:42:20 -0500

    chore(internal): further delete things from addon-docs

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,21 +1,10 @@
-import config from 'dummy/config/environment';
-import AddonDocsRouter, { docsRoute } from 'ember-cli-addon-docs/router';
+import EmberRouter from '@ember/routing/router';
 
-export default class Router extends AddonDocsRouter {
+import config from 'dummy/config/environment';
+
+export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function () {
-  docsRoute(this, function () {
-    /* Your docs routes go here */
-    this.route('usage');
-    this.route('single-camera');
-    this.route('multiple-cameras');
-    this.route('testing');
-    this.route('loading');
-    this.route('attach-qr-scanner');
-  });
-
-  this.route('not-found', { path: '/*path' });
-});
+Router.map(function () {});
